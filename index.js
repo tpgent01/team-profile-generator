0dfdb12d3d1e78b6c240faa0354b8b0fd9ba9276
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-02-22 17:23:25 -0600

    added inquirer functions in index.js

--- index.js
+++ index.js
@@ -1,21 +1,117 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-inquirer.prompt([
-    {
-        message: "Please enter your name",
-        name: "username"
-    },
-    {
-        message: "How many people are on your team?",
-        name: "teamSize"
-    },
-    {
-        message: "Please enter your name",
-        name: "username"
-    },
-])
+const employee = require("./lib/employee")
+const engineer = require("./lib/engineer")
+const manager = require("./lib/manager")
+const intern = require("./lib/intern")
 
-.then(function(data){
-    console.log(data.teamSize)
-});
+let finalTeamArray = [];
+
+function startPrompt() {
+    inquirer.prompt([
+        {
+            message: "What is your team manager's name?",
+            name: "name"
+        },
+        {
+            message: "What is your team manager's email address?",
+            name: "email"
+        },
+
+        {
+            type: "number",
+            message: "What is your team manager's office number?",
+            name: "officeNumber"
+        }
+    ])
+
+    .then(function (data) {
+        console.log(data)
+        addTeamMembers();
+    });
+}
+
+function addTeamMembers() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Would you like to add more team members?",
+            choices: ["Yes, add an engineer", "Yes, add an intern", "No, my team is complete"],
+            name: "addMemberData"
+        }
+    ])
+
+    .then(function (data) {
+        console.log(data.addMemberData)
+
+        switch (data.addMemberData) {
+            case "Yes, add an engineer":
+                    addEngineer();
+                    break;
+
+                case "Yes, add an intern":
+                    addIntern();
+                    break;
+                case "No, my team is complete":
+                    compileTeam();
+                    break;
+            }
+        });
+}
+
+function addEngineer() {
+    inquirer.prompt([
+        {
+            message: "What is this engineer's name?",
+            name: "name"
+        },
+        {
+            message: "What is this engineer's email address?",
+            name: "email"
+        },
+        {
+            message: "What is this engineer's Github profile?",
+            name: "github"
+        }
+    ])
+
+        .then(function (data) {
+            console.log(data.name, data.email, data.github)
+            addTeamMembers()
+
+        });
+
+};
+
+function addIntern() {
+    inquirer.prompt([
+        {
+            message: "What is this intern's name?",
+            name: "name"
+        },
+        {
+            message: "What is this intern's email address?",
+            name: "email"
+        },
+        {
+            message: "What is this intern's school?",
+            name: "school"
+        }
+    ])
+
+        .then(function (data) {
+            console.log(data.name, data.email, data.school)
+            addTeamMembers()
+
+        });
+
+};
+
+function compileTeam(){
+    console.log("complete!")
+}
+
+
+
+startingPrompt();
