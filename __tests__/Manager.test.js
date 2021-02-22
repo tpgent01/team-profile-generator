09fada874e5b4aad34df002c562302f6cb51741d
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-02-22 16:51:34 -0600

    manager tests pass / engineer tests fail

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -16,5 +16,5 @@ test("Can set office number via constructor argrument", () => {
   test("Can get office number via getOffice()", () => {
     const testValue = 100;
     const e = new Manager("Foo", 1, "test@test.com", testValue);
-    expect(e.officeNumber()).toBe(testValue);
+    expect(e.getOfficeNumber()).toBe(testValue);
   });
